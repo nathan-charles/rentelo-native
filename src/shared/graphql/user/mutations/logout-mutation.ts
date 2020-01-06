cc390327c5fe5b449c8ef78361536d24ecae008c
Nathan Charles <nathan@nathancharles.com>
2020-01-05 23:46:43 -0600

    Update logout-mutation.ts

--- src/shared/graphql/user/mutations/logout-mutation.ts
+++ src/shared/graphql/user/mutations/logout-mutation.ts
@@ -3,12 +3,12 @@ import gql from 'graphql-tag';
 const LOGOUT_MUTATION = gql`
   mutation LogoutMutation {
     logOut {
-      id
-      updatedAt
-      createdAt
-      username
+      # id
+      # updatedAt
+      # createdAt
+      # username
       sessionToken
-      ACL
+      # ACL
     }
   }
 `;
